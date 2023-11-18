80afff47124fdc0a9a3295fb39b864f9f34212af
Sibongile <sibongile.nhlema@gmail.com>
2023-11-18 08:42:50 -0800

    saving progress

--- factor.c
+++ factor.c
@@ -11,15 +11,15 @@ void factorise(int n)
 
 	if (n % 2 == 0)
 	{
-		printf("%d=%d*%d\n", n, 2, n / 2);
+		printf("%d=%d*%d\n", n, n / 2, 2);
 		n /= 2;
 	}
 	for (p = 3; p <= sqrt(n); p += 2)
 	{
-		while (n % p == 0)
+		if (n % p == 0)
 		{
 			q = n / p;
-			printf("%d=%d*%d\n", n, p, q);
+			printf("%d=%d*%d\n", n, q, p);
 			n = q;
 		}
 	}
